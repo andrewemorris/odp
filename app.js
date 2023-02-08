51011b798d00efffe961ccb98bdd69dcc7feeb2c
Andrew Morris <andrew.morris.crypto@gmail.com>
2023-02-08 18:49:09 +0000

    fix images

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ const productNames = [
   "water-can",
   "wine-glass",
 ];
+/*
+let image1 = document.querySelector("section img:first-child");
+let image2 = document.querySelector("section img:nth-child(2)");
+let image3 = document.querySelector("section img:nth-child(2)");
+*/
 
 function Product(name, src) {
   this.name = name;
@@ -30,5 +35,34 @@ function Product(name, src) {
 Product.allProducts = [];
 
 for (let i = 0; i < productNames.length; i++) {
-  new Product(productNames[i], "images/" + productNames[i]);
+  new Product(productNames[i], `images/${productNames[i]}.jpeg`);
 }
+
+function randomIndex() {
+  return Math.floor(Math.random() * Product.allProducts.length);
+}
+
+function renderImages() {
+  let ind1 = randomIndex();
+  let ind2 = randomIndex();
+  let ind3 = randomIndex();
+
+  while (ind1 === ind2 || ind1 === ind2 || ind1 == ind3) {
+    let ind1 = randomIndex();
+    let ind2 = randomIndex();
+  };
+
+  let img1 = document.getElementById("img1");
+  let img2 = document.getElementById("img2");
+  let img3 = document.getElementById("img3");
+
+  img1.src = Product.allProducts[ind1].src;
+  img2.src = Product.allProducts[ind2].src;
+  img3.src = Product.allProducts[ind3].src;
+
+  Product.allProductsArray[product1].views++;
+  Product.allProductsArray[product2].views++;
+  Product.allProductsArray[product3].views++;
+}
+
+renderImages();
